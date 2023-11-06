56dc36e882997639fc4f961ac23f8a8191ad0a1c
Basawraj07 <basumat323@gmail.com>
2023-11-06 15:09:22 +0530

    tst

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import Header from './components/Header'
-import Landing from './components/Landing'
-import Service from './components/Service'
-import Footer from './components/Footer'
-let App = ()=>{
+import React from 'react'
 
-
-    return <div>
-       
-         <nav className='navbar navbar-dark bg-dark'>
-            <a href="#" className='navbar-brand'>Bootstrap Example</a>
-         </nav>
-         <hr />
-          <h1>App Componnet</h1>
-        <hr />
-        <Header/>
-        <Landing/>
-        <Service/>
-        <Footer/>
-
-    </div>
+    const name = 'basu'
+    const loc = 'Goa'
+    const currDate = new Date().toLocaleDateString();
+    const currTime = new Date().toLocaleTimeString();
+function Calender(){
+    return <>
+    
+    <h1 className='.header'>HELLO</h1>
+    <h2>My Name Is {name}</h2>
+    <h2>I Am From {loc}</h2>
+    <h3>Current Date Is:{currDate}</h3>
+    <h3>Current Date Is:{currTime}</h3>
+    </>
 }
-
-export default App
+export default Calender
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom'
-import App from './App'
-/* import '../node_modules/bootstrap/dist/css/bootstrap.css' */
-//ReactDOM.render("Hello,GM",document.getElementById('root'))
-ReactDOM.render(<App/>,document.getElementById('root'))
+import Calender from './App';
+import "./index.css"
+
+ReactDOM.render(<Calender/>,document.getElementById('root'));
